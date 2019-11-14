96cd10752e6ef5f470f9e8f82b0af867dceaa711
saikatjaman <saikatjaman@gmail.com>
2019-11-15 01:23:59 +0800

    add todoapp

---
+++ blog/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from blog.models import BlogPost
+
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'body', 'image','date_updated']
